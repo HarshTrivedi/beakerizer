ec7328acafa3ff07da63c7bb8123915f4aae32b9
Harsh Trivedi <harshjtrivedi94@gmail.com>
2022-09-08 00:10:54 -0400

    fix download script.

--- download.py
+++ download.py
@@ -30,7 +30,7 @@ def get_experiments_results_dataset_ids(beaker_experiment_name: str) -> List[str
     ).strip()
     experiment_details = json.loads(experiment_details)
     results_dataset_ids = [
-        task_obj["result"]["beaker"] for task_obj in experiment_details[0]["executions"]
+        task_obj["execution"]["result"]["beaker"] for task_obj in experiment_details[0]["jobs"]
     ]
     return results_dataset_ids
 
@@ -39,16 +39,22 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument("experiment_name", type=str, help="Experiment name.")
+    parser.add_argument(
+        "--dry-run",
+        action="store_true",
+        default=False,
+        help="If specified, the download commands will only be printed and not executed.",
+    )
     args = parser.parse_args()
 
-    experiment_config_jsonnet_path = os.path.join(
-        "experiment_configs", "configs", args.experiment_name + ".jsonnet"
+    experiment_config_path = os.path.join(
+        "experiment_configs", args.experiment_name + ".jsonnet"
     )
-    if not os.path.exists(experiment_config_jsonnet_path):
-        exit("Neither jsonnet or json experiment config found.")
+    if not os.path.exists(experiment_config_path):
+        exit("Experiment config found.")
 
     experiment_config = json.loads(
-        _jsonnet.evaluate_file(experiment_config_jsonnet_path)
+        _jsonnet.evaluate_file(experiment_config_path)
     )
 
     output_directory = experiment_config["local_output_directory"]
@@ -57,10 +63,11 @@ def main():
     results_dataset_ids = get_experiments_results_dataset_ids(beaker_experiment_name)
 
     if len(results_dataset_ids) > 1:
-        assert "$INDEX" in output_directory
+        if "$INDEX" not in output_directory:
+            print(f"WARNING: Will download all results in the same output_directory: {output_directory}")
         output_directories = [
             output_directory.replace("$INDEX", str(index))
-            for index in len(results_dataset_ids)
+            for index in range(len(results_dataset_ids))
         ]
     else:
         assert "$INDEX" not in output_directory
@@ -72,10 +79,12 @@ def main():
         zip(output_directories, results_dataset_ids)
     ):
         print(f"Downloading results {index+1}/{len(results_dataset_ids)} in {output_directory}")
-        command = (
-            f"beaker dataset fetch --output {output_directory} {dataset_id}"
-        )
-
+        command = [
+            "beaker", "dataset", "fetch", "--output", output_directory, dataset_id
+        ]
+        print(" ".join(command))
+        if not args.dry_run:
+            subprocess.run(command)
 
 if __name__ == "__main__":
     main()
